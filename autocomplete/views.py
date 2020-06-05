dca43e8ef5952c5eb374f18d955260da03e025f5
Thierry Chappuis <thierry@chappuis.io>
2020-06-05 14:08:39 +0200

    Added a bug

--- autocomplete/views.py
+++ autocomplete/views.py
@@ -6,6 +6,6 @@ from products.models import Product
 
 def complete(request):
     searched_term = request.GET.get("term")
-    product = Product.objects.get_all_by_term(searched_term)
-    product = [product.name for product in product]
-    return JsonResponse(product, safe=False)
+    products = Product.objects.filter(product_name_fr__icontains=searched_term)
+    autocomplete_products = [products.product_name_fr for products in products][:10]
+    return JsonResponse(autocomplete_products, safe=False)
